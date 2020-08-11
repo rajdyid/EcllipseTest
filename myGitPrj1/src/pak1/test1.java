dc8f57b052a7843022366fe97fc7566ed13b42a3
Vaidya <Vaidya@SUPERMAN-VAIDYA>
2020-08-11 18:06:00 +0530

    First commit

---
+++ myGitPrj1/src/pak1/test1.java
@@ -0,0 +1,12 @@
+package pak1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x, y;
+		x=10; y=55;
+		int z = x + y;
+		System.out.println(z);
+	}
+}
